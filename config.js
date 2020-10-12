99debedf182f0c0de6a94e03ece5622a982106c4
Dariusz Dobosz <dareq112@gmail.com>
2020-10-12 20:12:25 +0200

    news form

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://admin:admin@cluster0.pqai8.mongodb.net/cluster0?retryWrites=true&w=majority',
+    keySession: ['TWOJKLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+}
